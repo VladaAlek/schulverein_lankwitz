60835c6a7bc0d22c52ace4cd940d65ddcccb5d28
Vladimir Aleksic <vladimiraleksic.nis@gmail.com>
2025-07-25 08:15:12 +0000

    feat: create GallerryImage model, register and migrate it

--- galerie/admin.py
+++ galerie/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import GalleryImage
+
+admin.site.register(GalleryImage)
--- galerie/models.py
+++ galerie/models.py
@@ -1,2 +1,13 @@
+from django.db import models
+
+class GalleryImage(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='gallery/')
+    start_date = models.DateField()
+    end_date = models.DateField()
+    place = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
 
 
