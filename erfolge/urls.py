35cf58300d3ee900e10fd7b228d8e86aab73fac4
Vladimir Aleksic <vladimiraleksic.nis@gmail.com>
2025-07-25 13:56:02 +0000

    refactor the erfolg.html structure and basic style

--- erfolge/urls.py
+++ erfolge/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [ 
-    path('', views.erfolge, name= "erfolge")
+    path('', views.erfolge_view, name= "erfolge")
 ]
--- erfolge/views.py
+++ erfolge/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Erfolge
 
-def erfolge(request):
-    return render(request, 'erfolge/erfolge.html')
+def erfolge_view(request):
+    erfolge = Erfolge.objects.all()  # Use 'erfolge' for both context and template
+    return render(request, 'erfolge/erfolge.html', {'erfolge': erfolge})
